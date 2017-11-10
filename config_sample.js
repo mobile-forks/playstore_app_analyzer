dfb326b702262c8dc26d3113f1d2f34e23f5159b
Michael <migamiga@web.de>
2017-11-10 21:57:04 +0100

    fix config_sample

--- config_sample.js
+++ config_sample.js
@@ -5,7 +5,7 @@ exports.androidId = "android_id";
 
 // playstore settings
 // https://github.com/facundoolano/google-play-scraper/blob/dev/lib/constants.js#L3
-exports.category = gplay.category.AUTO_AND_VEHICLES;
-exports.collection = gplay.collection.NEW_FREE;
+exports.category = 'EVENTS';
+exports.collection = 'NEW_FREE';
 exports.downloadCount = 40;
 exports.country = "de";
